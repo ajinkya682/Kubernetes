2ae5141590539beb044a017ba4666788f71fb906
Ajinkya Saivar <172878214+ajinkya682@users.noreply.github.com>
2026-05-11 23:02:54 +0530

    Initialize backend with Express and Morgan; set up basic server and app structure

---
+++ Backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("Dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    sum += 1;
+  }
+  res.send(`Hello World! ${sum}`);
+});
+
+export default app;
